55bdcab2531a86426c6ee2171cd725f66c4afb22
pd766 <wpd16818743@gmail.com>
2025-12-05 21:24:47 +0800

    fix for latest comfyui

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
 # required for ComfyUI custom node package
+
+from .split_text_by_emptyline import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
